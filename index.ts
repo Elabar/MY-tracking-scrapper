0e220f20c6838804b4932652287815709488b033
Lee Hoe Mun <leehoemun@gmail.com>
2021-07-01 00:02:49 +0800

    chore: tidy up index file

--- index.ts
+++ index.ts
@@ -1,15 +1,13 @@
 import { trackJNT } from "./trackers/jnt";
 import { trackPoslaju } from "./trackers/poslaju";
 
-
-
 const start = async () => {
-  // const jntTrackings = ["630367278709", "630367278692"];
-  // console.time("get_jnt_time");
-  // console.log(`Getting J&T tracking with: ${jntTrackings}`);
-  // const JNTResult = await trackJNT(jntTrackings);
-  // console.log(JSON.stringify(JNTResult, null, 2));
-  // console.timeEnd("get_jnt_time");
+  const jntTrackings = ["630367278709", "630367278692"];
+  console.time("get_jnt_time");
+  console.log(`Getting J&T tracking with: ${jntTrackings}`);
+  const JNTResult = await trackJNT(jntTrackings);
+  console.log(JSON.stringify(JNTResult, null, 2));
+  console.timeEnd("get_jnt_time");
 
   const poslajuTrackings = ["PL764441620465", "PL002889373280"];
   console.time("get_poslaju_time");
@@ -17,8 +15,6 @@ const start = async () => {
   const poslajuResult = await trackPoslaju(poslajuTrackings);
   console.log(JSON.stringify(poslajuResult, null, 2));
   console.timeEnd("get_poslaju_time");
-
-
 };
 
 start();
